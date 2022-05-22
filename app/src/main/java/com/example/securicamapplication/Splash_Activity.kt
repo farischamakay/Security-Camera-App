67ec0a090e36329f799a2e928dec0b452df6736d
Farischa Makay <farischamakay09@gmail.com>
2022-05-22 22:59:16 +0700

    Baru buat Splash Screen

---
+++ app/src/main/java/com/example/securicamapplication/Splash_Activity.kt
@@ -0,0 +1,32 @@
+package com.example.securicamapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.securicamapplication.databinding.ActivitySplashBinding
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash_Activity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
+    private val handler = Handler(Looper.getMainLooper())
+
+    private var valueDelay = 3500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        val actionbar = supportActionBar
+
+        actionbar?.title = " "
+
+
+        handler.postDelayed({
+            val splashIntent = Intent(this, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }, valueDelay)
+    }
+}
